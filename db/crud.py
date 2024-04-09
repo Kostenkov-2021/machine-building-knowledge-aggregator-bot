07d322e99537230a8bf7bab2ceb8311578467d65
deniz <deniz@r1oaz.ru>
2024-04-09 22:25:06 +0300

    Fixed bugs in crud

--- db/crud.py
+++ db/crud.py
@@ -16,8 +16,7 @@ def create_knowledge_request(db: Session, user_id: int, content: str):
 
 
 def edit_knowledge_request(db: Session, request_id: int, content: str):
-    db_request = select(models.KnowledgeRequest).where(
-        models.KnowledgeRequest.id == request_id).first()
+    db_request = db.execute(select(models.KnowledgeRequest).where(models.KnowledgeRequest.id == request_id)).scalars().first()
     db_request.content = content
     db.commit()
     db.refresh(db_request)
@@ -25,11 +24,11 @@ def edit_knowledge_request(db: Session, request_id: int, content: str):
 
 
 def get_knowledge_request(db: Session, request_id: int):
-    return db.execute(select(models.KnowledgeRequest).where(models.KnowledgeRequest.id == request_id)).first()
+    return db.execute(select(models.KnowledgeRequest).where(models.KnowledgeRequest.id == request_id)).scalars().first()
 
 
 def get_knowledge_requests(db: Session):
-    return db.execute(select(models.KnowledgeRequest).order_by(models.KnowledgeRequest.timestamp.desc())).all()
+    return db.execute(select(models.KnowledgeRequest).order_by(models.KnowledgeRequest.timestamp.desc())).scalars().all()
 
 
 def add_response_to_request(db: Session, request_id: int, user_id: int, content: str):
@@ -42,7 +41,7 @@ def add_response_to_request(db: Session, request_id: int, user_id: int, content:
 
 
 def edit_response(db: Session, response_id: int, content: str):
-    db_response=select(models.Response).where(models.Response.id == response_id).first()
+    db_response=db.execute(select(models.Response).where(models.Response.id == response_id)).scalars().first()
     db_response.content = content
     db.commit()
     db.refresh(db_response)
@@ -50,7 +49,7 @@ def edit_response(db: Session, response_id: int, content: str):
 
 
 def get_responses_for_request(db: Session, request_id: int):  #using select instead of query
-    return db.execute(select(models.Response).where(models.Response.request_id == request_id).order_by(models.Response.timestamp.asc())).all()
+    return db.execute(select(models.Response).where(models.Response.request_id == request_id).order_by(models.Response.timestamp.asc())).scalars().all()
 
 
 def create_user(db: Session, tg_id: int, tg_name: str, name: str = None):
@@ -64,7 +63,7 @@ def create_user(db: Session, tg_id: int, tg_name: str, name: str = None):
 def get_user(db: Session, user_id: int = None, tg_id: int = None):
     query = select(models.User)
     if user_id:
-        return db.execute(query.where(models.User.id == user_id)).first()
+        return db.execute(query.where(models.User.id == user_id)).scalars().first()
     elif tg_id:
-        return db.execute(query.where(models.User.tg_id == tg_id)).first()
+        return db.execute(query.where(models.User.tg_id == tg_id)).scalars().first()
     return None
