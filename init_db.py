129c82c8b8d352890cb8c8a3433a53f94b77880b
deniz <deniz@r1oaz.ru>
2024-04-09 11:38:57 +0300

    init_db now showes the users table.

--- init_db.py
+++ init_db.py
@@ -1,10 +1,15 @@
-from db.database import engine
+from sqlalchemy import select
+from db.database import engine, SessionLocal
 from db.models import Base
 
 
 def create_database():
     Base.metadata.create_all(bind=engine)
 
+
 if __name__ == "__main__":
-    create_database()
-    print("Database and tables created successfully.")
+    create_database()    
+    print(f"Database and tables created successfully. Tables: {Base.metadata.tables.keys()}")
+    db=SessionLocal()
+    db.executes(select("users"))  # query object is now legacy. Use select() instead!
+    print(f"DBG: users table rows: {db}")
