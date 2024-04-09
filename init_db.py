a52d8431d4a9bc6b883c2bcc446ba5215493575b
deniz <deniz@r1oaz.ru>
2024-04-09 11:42:30 +0300

    fixed a typo in the init_db.py file

--- init_db.py
+++ init_db.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     create_database()    
     print(f"Database and tables created successfully. Tables: {Base.metadata.tables.keys()}")
     db=SessionLocal()
-    db.executes(select("users"))  # query object is now legacy. Use select() instead!
+    db.execute(select("users"))  # query object is now legacy. Use select() instead!
     print(f"DBG: users table rows: {db}")
