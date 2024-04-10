8d966d99d241990b593b477341520d8a21c9567b
deniz <deniz@r1oaz.ru>
2024-04-10 09:42:32 +0300

    Completed answer request handler part.

--- states/states.py
+++ states/states.py
@@ -3,6 +3,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 class States(StatesGroup):
     waiting_for_request_content = State()
-    waiting_for_answer_content = State()
+    waiting_for_response_content = State()
     waiting_for_edit_request_content = State()
     waiting_for_edit_response_content = State()
