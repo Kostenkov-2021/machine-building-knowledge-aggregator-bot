bfefe8382132f94f68b4457acd8f91e9fe6cd696
deniz <deniz@r1oaz.ru>
2024-04-10 09:16:56 +0300

    Added answer handling for the bot.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.fsm.strategy import FSMStrategy
 from handlers.start import start_router
 from handlers.knowledge_request import request_router
 from handlers.view_requests import view_requests_router
-
+from handlers.answer_request import answer_router
 
 API_TOKEN = os.getenv("MBKA_API_TOKEN", "forgot")
 if API_TOKEN=="forgot":
@@ -19,3 +19,4 @@ dp = Dispatcher(storage=MemoryStorage(), fsm_strategy=FSMStrategy.USER_IN_CHAT)
 dp.include_router(start_router)
 dp.include_router(request_router)
 dp.include_router(view_requests_router)
+dp.include_router(answer_router)
